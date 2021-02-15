57eaa8b96040addec2752a5db54c89c0df8783db
Aleksa Trampa <alektramp@gmail.com>
2021-02-15 14:45:26 +0100

    Script folders, player hit detect

--- Assets/_Project/Scripts/Mechanics/AttackHitDetector.cs
+++ Assets/_Project/Scripts/Mechanics/AttackHitDetector.cs
@@ -2,7 +2,7 @@
 
 public class AttackHitDetector : MonoBehaviour
 {
-    public void HitByAttack(AttackType attackType, float baseAttackDamage)
+    public virtual void HitByAttack(AttackType attackType, float baseAttackDamage)
     {
         print("Hit by attack of type: " + attackType + " with base damage of: " + baseAttackDamage);
     }
---
+++ Assets/_Project/Scripts/Player/PlayerHitByAttack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitByAttack : AttackHitDetector
+{
+    [SerializeField]private Health playerHealth;
+
+    private void Awake()
+    {
+        playerHealth = playerHealth == null? GetComponent<Health>() : playerHealth;
+    }
+
+    public override void HitByAttack(AttackType attackType, float baseAttackDamage)
+    {
+        base.HitByAttack(attackType, baseAttackDamage);
+        playerHealth.TakeDamage(baseAttackDamage);
+        print("The player was hit by an attack and took : " +  baseAttackDamage + " damage");
+    }
+}
