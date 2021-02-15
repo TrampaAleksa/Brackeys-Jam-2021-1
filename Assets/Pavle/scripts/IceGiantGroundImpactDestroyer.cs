194acc7889eb4a5bc022413d093d49c60c09179e
pajocar23 <56831069+pajocar23@users.noreply.github.com>
2021-02-15 22:16:55 +0100

    Ground area  which ice gigant smashes with fist,foot or weapon

---
+++ Assets/Pavle/scripts/IceGiantGroundImpactDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceGiantGroundImpactDestroyer : MonoBehaviour
+{
+    public float xSeconds = 3.6f;
+
+    private void Start()
+    {
+        StartCoroutine(waitAndDestroy());
+    }
+
+
+    private IEnumerator waitAndDestroy()
+    {
+        yield return new WaitForSeconds(xSeconds);
+        Destroy(transform.gameObject);
+    }
+}
