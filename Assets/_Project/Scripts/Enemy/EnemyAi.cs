7e1415e3ff98b4d93490ad42390884f833af0897
Aleksa Trampa <alektramp@gmail.com>
2021-02-15 20:52:03 +0100

    ally list static instance, enemy ai script with no error checks

--- Assets/_Project/Scripts/Enemy/EnemyAi.cs
+++ Assets/_Project/Scripts/Enemy/EnemyAi.cs
@@ -1,21 +1,60 @@
-﻿using UnityEngine;
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.AI;
 
 
 public class EnemyAi : MonoBehaviour
 {
+    public float timeBetweenAttacks;
+    
+    private AllyList allyList;
     private MeleeAttack meleeAttack;
-    public AttackHitDetector target;
+    private NavMeshAgent navMeshAgent;
+    
+    private bool attackInCooldown;
 
-    private void Awake()
+    void Awake()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>();
         meleeAttack = GetComponent<MeleeAttack>();
     }
 
-    [ContextMenu("Attack Closest Target")]
-    private void AttackClosestTarget()
+    private void Start()
     {
-        meleeAttack.LaunchAttack(target.transform);
+        allyList = AllyList.Instance;
     }
 
+    void Update()
+    {
+        // TryAttacking();
+    }
 
+    private void TryAttacking()
+    {
+        if (allyList.allies.Count != 0 && allyList.currentIndex < allyList.allies.Count)
+            navMeshAgent.SetDestination(allyList.allies[AllyList.Instance.currentIndex].transform.transform.position);
+        else return;
+
+        var inAttackRange = navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance;
+        if (!inAttackRange) return;
+
+        if (attackInCooldown) return;
+
+        attackInCooldown = true;
+
+        if (allyList.allies.Count == 0)
+        {
+            print("Game over");
+            return;
+        }
+
+        meleeAttack.LaunchAttack(allyList.allies[AllyList.Instance.currentIndex].transform);
+        gameObject.AddComponent<TimedAction>().StartTimedAction(FinishAttack, timeBetweenAttacks);
+    }
+
+    private void FinishAttack()
+    {
+        attackInCooldown = false;
+    }
 }
