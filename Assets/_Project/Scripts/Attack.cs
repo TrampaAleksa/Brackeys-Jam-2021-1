77603c37bd283adf273795a82d504ee6ac702a0c
Aleksa Trampa <alektramp@gmail.com>
2021-02-15 14:06:20 +0100

    Added attack, heath and hit detector scripts

---
+++ Assets/_Project/Scripts/Attack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public AttackType type;
+    public float baseDamage;
+    
+    
+    public virtual void LaunchAttack(Transform target)
+    {
+        
+    }
+}
+
+
+public enum AttackType
+{
+    Fire,
+    Ice,
+    Warrior,
+    Gnome,
+    Wind,
+    Reaper,
+    Spider
+}
---
+++ Assets/_Project/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+
+    public virtual void TakeDamage(float damageAmount)
+    {
+        
+    }
+
+    public virtual void Heal(float healAmount)
+    {
+        
+    }
+}
+
