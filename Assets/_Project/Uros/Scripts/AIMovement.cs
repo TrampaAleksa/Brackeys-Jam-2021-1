af83af3efa4c23188ba1e0e27f9076e304ca5c11
urosspasenovic <urosspasenovic9@gmail.com>
2021-02-15 15:33:32 +0100

    AIMovement

---
+++ Assets/_Project/Uros/Scripts/AIMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMoveme : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(player.transform.position);
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/_Project/Uros/Scripts/AIMovementBattle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovementBattle : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+    [SerializeField]
+    GameObject player;
+    NavMeshAgent navMeshAgent;
+    public bool isRetreating = false;
+    GameObject lookAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ako se ne krece koristi look at
+        //transform.LookAt(target.transform);
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        //retreat
+        if (!isRetreating)
+        {
+            navMeshAgent.SetDestination(target.transform.transform.position);
+            lookAt = target;
+        }
+        else
+        {
+            navMeshAgent.SetDestination(player.transform.position);
+            lookAt = player;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            isRetreating = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+            isRetreating = false;
+         if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            transform.LookAt(lookAt.transform);
+    }
+
+}
