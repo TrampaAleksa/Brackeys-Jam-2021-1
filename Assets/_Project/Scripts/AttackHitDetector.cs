c88f9d7546b3866c182ef9421f86cb004fd41818
Aleksa Trampa <alektramp@gmail.com>
2021-02-15 14:36:05 +0100

    Enemy ai script for attacks and movement logic. Attack hit detector for detecting attavcks

--- Assets/_Project/Scripts/AttackHitDetector.cs
+++ Assets/_Project/Scripts/AttackHitDetector.cs
@@ -4,6 +4,6 @@ public class AttackHitDetector : MonoBehaviour
 {
     public void HitByAttack(AttackType attackType, float baseAttackDamage)
     {
-        print("Hit by attack of type: " + attackType + "with base damage of: " + baseAttackDamage);
+        print("Hit by attack of type: " + attackType + " with base damage of: " + baseAttackDamage);
     }
 }
