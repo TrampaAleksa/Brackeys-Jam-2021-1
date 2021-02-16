0d48f7d5593e925208dff6860fdc73eecba96920
Aleksa Trampa <alektramp@gmail.com>
2021-02-16 18:31:51 +0100

    spikes collider with damage to units

---
+++ Assets/_Project/Scripts/Enemy/IceSpikesCollider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSpikesCollider : MonoBehaviour
+{
+    public AttackType type;
+    public float baseAttackDamage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ally"))
+        {
+            other.GetComponent<Ally>().hitDetector.HitByAttack(type, baseAttackDamage);
+        }
+        
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<AttackHitDetector>().HitByAttack(type, baseAttackDamage);
+        }
+    }
+}
