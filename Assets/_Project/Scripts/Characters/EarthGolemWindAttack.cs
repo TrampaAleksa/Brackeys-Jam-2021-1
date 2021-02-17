72ce82e820e68d8f57945d1225fc4728c096de57
Aleksa Trampa <alektramp@gmail.com>
2021-02-17 23:26:31 +0100

    added earth golem with tornado attack

---
+++ Assets/_Project/Scripts/Characters/EarthGolemWindAttack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EarthGolemWindAttack : Attack
+{
+    public GameObject windObj;
+    public float intervalOfDamage;
+    
+    private TimedAction _timedAction;
+
+    private void Awake()
+    {
+        _timedAction = gameObject.AddComponent<TimedAction>().DestroyOnFinish(false);
+    }
+
+    public override void LaunchAttack(Transform target)
+    {
+        base.LaunchAttack(target);
+        print("Launching spike attack!");
+
+        Instantiate(windObj, transform);
+        _timedAction.StartTimedAction(DamageAllAllies, intervalOfDamage);
+    }
+
+    private void DamageAllAllies()
+    {
+        foreach (var ally in AllyList.Instance.allies)
+        {
+            ally.hitDetector.HitByAttack(type, baseDamage);
+        }
+        
+        _timedAction.StartTimedAction(DamageAllAllies, intervalOfDamage);
+    }
+    
+    
+}
