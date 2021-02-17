95d020c16e7828321b607e10289461d3caaeec24
Aleksa Trampa <alektramp@gmail.com>
2021-02-17 21:08:08 +0100

    mana pool added

---
+++ Assets/_Project/Scripts/Mechanics/ManaPool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaPool : MonoBehaviour
+{
+   public static ManaPool Instance;
+
+   public float remainingMana;
+   public float resurrectionCost;
+   public float retreatCost;
+
+   private void Awake()
+   {
+      Instance = this;
+   }
+
+
+   public void ReduceMana(float amount)
+   {
+      remainingMana -= amount;
+   }
+   
+   public void AddMana(float amount)
+   {
+      remainingMana += amount;
+   }
+
+   public void CastedResurrection()
+   {
+      ReduceMana(resurrectionCost);
+   }
+   
+   public void CastedRetreat()
+   {
+      ReduceMana(retreatCost);
+   }
+   
+}
