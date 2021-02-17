1eacca5600d01b52c47322ac8a0faac3eb5ef942
pajocar23 <56831069+pajocar23@users.noreply.github.com>
2021-02-17 15:20:12 +0100

    Tornado done(texture problem)
    Align the camera view so that the line on tornado cant be seen

---
+++ Assets/Pavle/tornado/TornadoInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoInteraction : MonoBehaviour
+{
+    public Transform tornadoCenter;
+    public float pullForce;
+    public float refreshRate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "box")
+        {
+            StartCoroutine(pullObject(other,true));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "box")
+        {
+            StartCoroutine(pullObject(other, false));
+        }/////////////
+    }
+
+    IEnumerator pullObject(Collider col,bool pull) 
+    {
+        if (pull)
+        {
+            Vector3 forceDirection = tornadoCenter.position - col.transform.position;
+            col.GetComponent<Rigidbody>().AddForce(forceDirection.normalized * pullForce * Time.deltaTime);
+            yield return refreshRate;
+            StartCoroutine(pullObject(col, true));
+        }
+    }
+}
