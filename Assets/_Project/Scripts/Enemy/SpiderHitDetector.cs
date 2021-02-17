bc6959327a5d105c69c0fd98e7d4b8a75dfe4f01
Aleksa Trampa <alektramp@gmail.com>
2021-02-18 00:27:19 +0100

    spider

---
+++ Assets/_Project/Scripts/Enemy/SpiderHitDetector.cs
@@ -0,0 +1,27 @@
+﻿public class SpiderHitDetector : AttackHitDetector
+{
+    private float damageReduction = 0.2f;
+    private Health _health;
+
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+    }
+
+    public override void HitByAttack(AttackType attackType, float baseAttackDamage)
+    {
+        var totalDamageTaken = baseAttackDamage;
+
+        if (attackType == AttackType.Fire)
+            totalDamageTaken *= damageReduction;
+        
+        _health.TakeDamage(totalDamageTaken);
+        print("Spider took: " + totalDamageTaken +  " damage");
+
+        if (_health.currentHealth <= 0.001f)
+        {
+            AllyList.Instance.ExitAllyCombat();
+            Destroy(gameObject);
+        }
+    }
+}
