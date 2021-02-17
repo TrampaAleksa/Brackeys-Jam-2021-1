7349060ec728c5921f552ca51b43e25aa32947ef
Aleksa Trampa <alektramp@gmail.com>
2021-02-18 00:09:12 +0100

    reaper

---
+++ Assets/_Project/Scripts/Characters/ReaperCleaveCollider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ReaperCleaveCollider : MonoBehaviour
+{
+    public AttackType type;
+    public float baseAttackDamage;
+
+    private Transform _cleaveTransform;
+    [SerializeField] private float movementSpeed;
+
+    private void Awake()
+    {
+        _cleaveTransform = transform;
+    }
+
+    private void Update()
+    {
+        _cleaveTransform.position += _cleaveTransform.forward * (movementSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ally"))
+        {
+            other.GetComponent<Ally>().hitDetector.HitByAttack(type, baseAttackDamage);
+        }
+    }
+}
