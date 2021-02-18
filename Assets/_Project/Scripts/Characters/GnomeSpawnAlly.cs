53bfdd111a82e1448ee090c8adb9fc32dd39e633
Aleksa Trampa <alektramp@gmail.com>
2021-02-18 21:54:59 +0100

    gnome spawn 5 at once instead of leader

---
+++ Assets/_Project/Scripts/Characters/GnomeSpawnAlly.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class GnomeSpawnAlly : MonoBehaviour
+{
+    public Ally gnomeAttacker;
+    public int gnomeLimit = 5;
+
+    private Transform _gnomeTransform;
+    private int _numberOfGnomes;
+    private void Update()
+    {
+        _gnomeTransform = transform;
+
+        for (int i = 0; i < gnomeLimit; i++)
+        {
+            var gnomeAttackedSpawned = Instantiate(gnomeAttacker, _gnomeTransform.position + gnomeAttacker.transform.position, _gnomeTransform.rotation);
+            AllyList.Instance.AllyRaised(gnomeAttackedSpawned);
+        }
+
+        AllyList.Instance.allies.Remove(GetComponent<Ally>());
+        Destroy(gameObject);
+    }
+}
