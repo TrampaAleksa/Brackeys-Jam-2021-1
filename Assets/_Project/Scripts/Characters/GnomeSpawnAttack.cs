be6cc2eb815c5604d35f603368a8612745a2babf
Aleksa Trampa <alektramp@gmail.com>
2021-02-18 01:12:05 +0100

    gnome limit, still overpowered

--- Assets/_Project/Scripts/Characters/GnomeSpawnAttack.cs
+++ Assets/_Project/Scripts/Characters/GnomeSpawnAttack.cs
@@ -1,16 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GnomeSpawnAttack : Attack
 {
     public Ally gnomeAttacker;
+    public int gnomeLimit = 5;
+
+    private Transform _gnomeTransform;
+    private int _numberOfGnomes;
+    private void Awake()
+    {
+        _gnomeTransform = transform;
+    }
+
     public override void LaunchAttack(Transform target)
     {
+        if (_numberOfGnomes > gnomeLimit) return;
+
         base.LaunchAttack(target);
         print("Launching melee attack!");
-
-
-        var gnomeAttackedSpawned = Instantiate(gnomeAttacker, transform);
+        
+        var gnomeAttackedSpawned = Instantiate(gnomeAttacker, _gnomeTransform.position + gnomeAttacker.transform.position, _gnomeTransform.rotation);
         gnomeAttackedSpawned.movement.target = target.gameObject;
         gnomeAttackedSpawned.movement.state = AllyMovementState.InBattle;
+
+        _numberOfGnomes++;
     }
 }
