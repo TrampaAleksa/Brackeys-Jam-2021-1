dd7c18290e2c097253b8ea33e667deca20b0cf0e
Aleksa Trampa <alektramp@gmail.com>
2021-02-19 12:46:19 +0100

    Health bar object and camera allign script

---
+++ Assets/_Project/Scripts/Utility/HealthBarCameraAlligner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarCameraAlligner : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void AlignCamera() {
+            var camXform = mainCamera.transform;
+            var forward = transform.position - camXform.position;
+            forward.Normalize();
+            var up = Vector3.Cross(forward, camXform.right);
+            transform.rotation = Quaternion.LookRotation(forward, up);
+    }
+
+    private void Update()
+    {
+        AlignCamera();
+    }
+}
