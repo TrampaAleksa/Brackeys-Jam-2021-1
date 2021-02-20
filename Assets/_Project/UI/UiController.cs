c61f9cbc3b3ba18ea7572c2d3e4089d7c32acdf7
Aleksa Trampa <alektramp@gmail.com>
2021-02-21 00:46:56 +0100

    ui controller complete with all needed methods

--- Assets/_Project/UI/UiController.cs
+++ Assets/_Project/UI/UiController.cs
@@ -3,15 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UiController : MonoBehaviour
 {
     public GameObject pausePanel;
-    public GameObject mainMenuPanel;
     
-    public Button musicButtonOn;
-    public Button musicButtonOff;
+    public GameObject musicButtonOn;
+    public GameObject musicButtonOff;
 
     public AudioMixer audioMixer;
 
@@ -47,15 +47,32 @@ public class UiController : MonoBehaviour
     [ContextMenu("toggle sound off")]
     public void ToggleSoundOff()
     {
+        musicButtonOn.SetActive(false);
+        musicButtonOff.SetActive(true);
         audioMixer.SetFloat("MasterVolume", -80);
     }
     
     [ContextMenu("toggle sound on")]
     public void ToggleSoundOn()
     {
+        musicButtonOn.SetActive(true);
+        musicButtonOff.SetActive(false);
         audioMixer.SetFloat("MasterVolume", 0);
     }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
