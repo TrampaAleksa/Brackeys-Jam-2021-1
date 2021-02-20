f8ee8e9279849e4dc7a90ce605edfd5df37f0af1
Aleksa Trampa <alektramp@gmail.com>
2021-02-21 00:17:26 +0100

    Ui controller stoop and unstop time methods

---
+++ Assets/_Project/UI/UiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+    [ContextMenu("Stop time")]
+    public void StopTime()
+    {
+        Time.timeScale = 0;
+        AudioHolder.Instance.PauseAllSounds();
+    }
+
+    [ContextMenu("Unstop time")]
+
+    public void UnstopTime()
+    {
+        Time.timeScale = 1;
+        AudioHolder.Instance.UnPauseAllSounds();
+
+    }
+}
