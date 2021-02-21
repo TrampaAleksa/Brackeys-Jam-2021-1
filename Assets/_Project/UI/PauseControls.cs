71261c7ae5f6b9e81f86256891ed4729ba913244
Aleksa Trampa <alektramp@gmail.com>
2021-02-21 09:13:39 +0100

    ui workingui prefabs and canvases

---
+++ Assets/_Project/UI/PauseControls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PauseControls : MonoBehaviour
+{
+    public GameObject pausePanel;
+    
+    [ContextMenu("Stop time")]
+    public void StopTime()
+    {
+        Time.timeScale = 0;
+        AudioHolder.Instance.PauseAllSounds();
+    }
+
+    
+    public void PauseGame()
+    {
+        StopTime();
+        pausePanel.SetActive(true);
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+}
